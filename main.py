6197b5e9070ba35b25a801f603bc2a76b12e17cd
Ashish <ashish2000patil@gmail.com>
2022-11-20 19:27:30 +0530

    [Ashish]Added: Add person

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ class Contacts:
         self.Phone_number = Phone_number
         self.Email = Email
 
+class Addressbook:
+    def __init__(self):
+        self.contacts = []
+
+    def add_person(self,con_obj):
+        self.contacts.append(con_obj)
+
+    def display(self):
+        for i in self.contacts:
+            print("First name: {}".format(i.First_name))
+            print("Last name: {}".format(i.Last_name))
+            print("Address: {}".format(i.Address))
+            print("State: {}".format(i.State))
+            print("City: {}".format(i.City))
+            print("Zip: {}".format(i.Zip))
+            print("Phone number: {}".format(i.Phone_number))
+            print("Email: {}".format(i.Email))
 
 if __name__ == '__main__':
-    print("Welcome to Address Book")
+
+    con = Contacts("Ashish","Patil","Sangli","MH","Sangli",416416,9156524636,"abc@gmail.com")
+    Add = Addressbook()
+    Add.add_person(con)
+    Add.display()
